278ba9686111b354c14454f04b5b4840ee3c862b
metlx <towers1337@gmail.com>
2022-01-17 18:23:57 -0500

    like some configs bro

---
+++ weaponx.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo -e "\e[31mMake sure you run in your home directory.\e[0m"
+sleep 3
+git clone https://github.com/VundleVim/Vundle.vim.git
+git clone https://github.com/metlx/metlx-vim.git
+git clone https://github.com/metlx/dots
+echo -e "\e[90mSetting things up...\e[0m"
+sleep 3
+mv ~/metlx-vim/.vim ~/
+mkdir ~/.vim/bundle
+mv ~/Vundle.vim ~/.vim/bundle/Vundle.vim
+mv ~/metlx-vim/.vimrc ~/
+mv ~/dots/.tmux.conf ~/
+echo -e "\e[32mdone!\e[0m"
+echo -e "\e[31mCleaning up...\e[0m"
+sleep 3
+rm -rf ~/dots
+rm -rf ~/metlx-vim
+rm -rf ~/weaponx.sh
+echo -e "\e[32mdone!\e[0m"
